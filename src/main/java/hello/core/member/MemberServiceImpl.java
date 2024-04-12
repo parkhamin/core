b2b5d79e1ffb3e37d0326a406449d2b106090b20
박하민 <parkhamint@naver.com>
2024-04-13 02:17:31 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     private final MemberRepository memberRepository;
 
+    // 의존관계 자동 주입해주는 AutoWired라는 기능을 생성자에 붙여준다.
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,11 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPoliciy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPoliciy discountPoliciy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPoliciy discountPoliciy) {
         this.memberRepository = memberRepository;
         this.discountPoliciy = discountPoliciy;
