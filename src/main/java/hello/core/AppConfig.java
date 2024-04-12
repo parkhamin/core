86ebe1bfabdd3950ec556405a455f990174cef4b
박하민 <parkhamint@naver.com>
2024-04-13 01:28:24 +0900

    MemberRepository -> memberRepository (대소문자 오타 수정)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,21 +13,25 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+    //@Bean memberService -> MemoryMemberRepository()
+    //@Bean orderSerice -> MemoryMemberRepository()
+    // 이로써 new가 2번이나 호출됨. 그러면 싱글톤이 깨지는 것 아닐까?
+
     // 각 메서드에 @Bean 적어주기 -> 그러면 spring container에 등록이 된다.
     @Bean
     public MemberService memberService(){
-        return new MemberServiceImpl(MemberRepository());
+        return new MemberServiceImpl(memberRepository());
         // 생성자 주입
     }
 
     @Bean
-    public static MemberRepository MemberRepository() {
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(MemberRepository(), discountPoliciy());
+        return new OrderServiceImpl(memberRepository(), discountPoliciy());
     }
 
     @Bean
