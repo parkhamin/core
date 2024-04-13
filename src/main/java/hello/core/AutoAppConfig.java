0e6c6ecc94b7eb3f0518b08420df37595edc0fa1
박하민 <parkhamint@naver.com>
2024-04-13 20:11:14 +0900

    중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+/*
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+*/
+    // 자동 빈 등록을 하려는데 수동 빈 등록하는 빈 이름과 동일하기 때문에 충돌이 난다.
+    // 자동 빈 등록과 수동 빈 등록 중에서는 수동 빈 등록이 우선권을 가진다.
 }
